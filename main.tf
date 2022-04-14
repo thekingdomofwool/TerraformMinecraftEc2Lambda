0eddabb2839be7394defe519c27ebbf03f21dff4
thekingdomofwool <103598369+thekingdomofwool@users.noreply.github.com>
2022-04-14 15:15:40 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "minecraft_server-sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["73.14.54.118/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   
   }
  
